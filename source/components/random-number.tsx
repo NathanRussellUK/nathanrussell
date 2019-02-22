e900e2e9a0a01993f352824779f4257ae27d13cd
Nathan Russell <professional@nathanrussell.me.uk>
2019-02-22 13:02:16 +0000

    Use memo in random number component, and delay in saga

--- source/components/random-number.tsx
+++ source/components/random-number.tsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { Dispatch } from "redux";
 
 import { State } from "../redux/state";
-import { getRandomNumber } from "../redux/duck-eggs/random-number";
+import { getRandomNumber } from '../redux/duck-eggs/random-number';
 import { duckHooks } from "../redux/hooks";
 
 export const RandomNumber: React.FunctionComponent = props => {
@@ -11,12 +11,45 @@ export const RandomNumber: React.FunctionComponent = props => {
 
     const randomNumber = duckHooks.system.randomNumber.useState();
 
-    // the second parameter specifies which props changing should trigger the effect to run again;
-    // as no props are provided, the effect will only run once, on the initial render
-    // this is the equivalent of componentWillMount
-    React.useEffect(() => {getRandomNumber({})}, []);
+    // the second parameter specifies which props changing should trigger the inner function to be called again;
+    const buttonContent = React.useMemo(() => {
+        switch (randomNumber.status) {
+            case "pending": {
+                return ""
+            }
+
+            case "success": {
+                return ""
+            }
+
+            default: {
+                return "Start!"
+            }
+        }
+    }, [randomNumber.status])
+
+    const displayContent = React.useMemo(() => {
+        switch (randomNumber.status) {
+            case "pending": {
+                return <div className="hold">One sec!</div>
+            }
+
+            case "success": {
+                return <div className="result">{`${randomNumber.data}!`}</div>
+            }
+
+            default: {
+                return <div className="prompt">Get random number?</div>
+            }
+        }
+    }, [randomNumber.status])
 
     return <div className="random-number">
-        <p>Random Number: {randomNumber || "Not yet fetched..."}</p>
+        <div className="display">
+            {displayContent}
+        </div>
+        <button onClick={() => { getRandomNumber({}) }}>
+            {buttonContent}
+        </button>
     </div>;
 }
--- source/redux/duck-eggs/random-number.ts
+++ source/redux/duck-eggs/random-number.ts
@@ -1,9 +1,10 @@
-import { put, takeEvery } from "redux-saga/effects";
+import { put, takeEvery, call } from "redux-saga/effects";
 import { actionCreatorFactory, ActionUnion } from "../action-creators";
+import { delay } from "redux-saga";
 
 export const getRandomNumber = actionCreatorFactory("getRandomNumber")();
 
-export const setRandomNumber = actionCreatorFactory("setRandomNumber")<{payload: number}>();
+export const setRandomNumber = actionCreatorFactory("setRandomNumber")<{ payload: number }>();
 
 const actionCreatorRefs = {
     getRandomNumber,
@@ -12,28 +13,41 @@ const actionCreatorRefs = {
 
 type DuckActionUnion = ActionUnion<typeof actionCreatorRefs>;
 
-export const reducer = (state: number = null, action: DuckActionUnion) => {
+type State = {
+    status?: "pending" | "success";
+    data?: number;
+};
+
+const defaultState: State = {};
+
+export const reducer = (state: State = defaultState, action: DuckActionUnion): State => {
     switch (action.type) {
         case "setRandomNumber": {
-            return action.payload
+            return {
+                status: "success",
+                data: action.payload
+            };
         }
-        case "getRandomNumber":
+        case "getRandomNumber": {
+            return { status: "pending" };
+        }
         default: {
-            return state
+            return state;
         }
     }
-}
+};
 
 export function* getRandomNumberSaga(action: ReturnType<typeof getRandomNumber.create>) {
+    yield call(delay, 2000);
     yield put(setRandomNumber.create({ payload: Math.random() }));
-}
-  
+};
+
 export function* rootSaga() {
     yield takeEvery(getRandomNumber.type, getRandomNumberSaga);
-}
+};
 
 export const randomNumber = {
     reducer,
     rootSaga,
     actions: actionCreatorRefs
-}
+};
