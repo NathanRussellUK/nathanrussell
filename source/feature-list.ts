887a4d22d7726c11d39d7486e831710b2f77edd0
Nathan Russell <dev@nathanrussell.me.uk>
2019-03-16 20:06:56 +0000

    Fix urls.

--- source/feature-list.ts
+++ source/feature-list.ts
@@ -4,43 +4,43 @@ export const featureList: FeatureProps[] = [
     {
         id: "typescript",
         to: "https://www.typescriptlang.org/",
-        src: "images/typescript.png",
+        src: "/images/typescript.png",
         alt: "TypeScript"
     },
     {
         id: "react",
         to: "https://reactjs.org/",
-        src: "images/react.svg",
+        src: "/images/react.svg",
         alt: "React"
     },
     {
         id: "redux-saga",
         to: "https://redux-saga.js.org/",
-        src: "images/redux-saga.svg",
+        src: "/images/redux-saga.svg",
         alt: "Redux Saga"
     },
     {
         id: "sass",
         to: "https://sass-lang.com/",
-        src: "images/sass.svg",
+        src: "/images/sass.svg",
         alt: "Sass"
     },
     {
         id: "cordova",
         to: "https://cordova.apache.org/",
-        src: "images/cordova.png",
+        src: "/images/cordova.png",
         alt: "Cordova"
     },
     {
         id: "git",
         to: "https://git-scm.com/",
-        src: "images/git.svg",
+        src: "/images/git.svg",
         alt: "Git"
     },
     {
         id: "node",
         to: "https://nodejs.org/en/",
-        src: "images/node.png",
+        src: "/images/node.png",
         alt: "Node"
     }
 ]
